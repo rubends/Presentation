44be05008c8eb57e2210133782fec5270788d9ec
rubends <rubendeswaef@gmail.com>
2015-10-28 13:46:06 +0100

    afbeelding gelukt

--- Presentation/ImageView.Designer.cs
+++ Presentation/ImageView.Designer.cs
@@ -39,7 +39,7 @@
       this.pictureBox1.Location = new System.Drawing.Point(3, 3);
       this.pictureBox1.Name = "pictureBox1";
       this.pictureBox1.Size = new System.Drawing.Size(144, 144);
-      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
       this.pictureBox1.TabIndex = 0;
       this.pictureBox1.TabStop = false;
       this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -50,9 +50,11 @@
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.Controls.Add(this.pictureBox1);
       this.Name = "ImageView";
+      this.Size = new System.Drawing.Size(461, 404);
       this.Load += new System.EventHandler(this.ImageView_Load);
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
