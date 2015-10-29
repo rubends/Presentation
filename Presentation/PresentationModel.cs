4f949a59dbcc1cbf35f2d0d04cbc9b32778d6f3f
AlexVanderbist <alex.vanderbist@gmail.com>
2015-10-29 14:13:03 +0100

    updating model

---
+++ Presentation/PresentationModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    public class PresentationModel
+    {
+        private PresentationController _controller;
+        private List<SlideController> _slides = new List<SlideController>();
+        private int _currentSlide = 0;
+        private SlideController _emptySlide;
+
+        public SlideController EmptySlide
+        {
+            get
+            {
+                return _emptySlide;
+            }
+            set
+            {
+                _emptySlide = value;
+            }
+        }
+
+        public PresentationModel(PresentationController cont)
+        {
+            _controller = cont;
+        }
+
+        public void addSlide(SlideController slideToAdd)
+        {
+            _slides.Add(slideToAdd);
+        }
+
+        public List<SlideController> Slides
+        {
+            get
+            {
+                return _slides;
+            }
+        }
+
+        public int CurrentSlide
+        {
+            get
+            {
+                return _currentSlide;
+            }
+            set
+            {
+                _currentSlide = value;
+            }
+        }
+
+    }
+}
